2a264bb61f6bc092d20d5732b9f99f4be76ed4a7
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-08-08 00:06:18 +0600

    feat(server): configure Better Auth with Google OAuth

---
+++ recipehub-server/auth.js
@@ -0,0 +1,40 @@
+import dns from "node:dns" ;
+dns.setServers (["1.1.1.1", "1.0.0.1"]);
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { client } from "./db.js";
+import dotenv from 'dotenv';
+import { Server } from "node:http";
+
+dotenv.config();
+
+const db = client.db(process.env.DB_NAME || "test");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+    usePlural: true,
+  }),
+  trustedOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+     "https://recipehub-client-delta.vercel.app" , 
+    process.env.CLIENT_URL
+  ].filter(Boolean),
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID ,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }
+  }
+});
